4c8f0f926240fc2706d5bb9740c3a5684e7e0be8
Matt <jonesm2@allegheny.edu>
2018-10-11 20:04:30 -0400

    Update Lab4Program.r

--- src/Lab4Program.r
+++ src/Lab4Program.r
@@ -38,10 +38,19 @@ ggplot(data=dat,mapping=aes(x= year, y = count2))+geom_bar(stat = "identity")
 hist(dat$count)
 hist(dat$count2)
 #Question 5.
+# This really is not the best answer in my mind, because the graph is harder to read than one would like.  Maybe another time we can try to create different panes, and seperate the data a little bit to understand the data a little better.
+ggplot(data = dat, mapping=aes(x = year, y = state, color = per_100s)) + geom_tile()
 
 #Question 6.
+#N/A
+#Question 7.
 
-
-
-
-
+# uses the data provided from the website cited in the writeup
+niaidDataOnEffectivenessOfVaccines <- data.frame(
+  disease = c("Measels", "Diphteria", "Mumps", "Pertussis", "Smallpox", "Rubella", "Haemophilus Influenzae", "Polio", "Tetanus"),
+  PreVaccineAnnualCases = c(505282, 175885, 152209, 147271, 48164, 47745, 20000, 16316, 1314),
+  CasesIn2009 = c(71, 0, 1991, 13214, 0, 3, 35, 0, 18),
+  PercentDecrease = c(99.9, 100, 98.7, 91.0, 100, 99.9, 99.8, 100, 98.6)
+)
+# blue is the annual cases before the vaccine, red is the number of cases in 2009, when the article was written.
+ggplot(data = niaidDataOnEffectivenessOfVaccines) + geom_point(mapping=aes(x = disease, y = PreVaccineAnnualCases), color = "blue") + geom_point(mapping=aes(x = disease, y = CasesIn2009), color = "red")
